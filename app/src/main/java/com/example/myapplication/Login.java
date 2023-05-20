ceb07dd338f21bea9d521d9c94a3e7e57425e8f1
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2023-05-21 00:53:49 +0530

    a

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -2,7 +2,14 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button login;
+        EditText email,pass;
+        email=findViewById(R.id.editTextTextEmailAddress2);
+        pass=findViewById(R.id.editTextTextPassword2);
+        login=findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Home.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
@@ -15,8 +16,9 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         Button register;
-        TextView email,pass;
+
         register=findViewById(R.id.button);
+        EditText email,pass;
         email=findViewById(R.id.editTextTextEmailAddress);
         pass=findViewById(R.id.editTextTextPassword);
         register.setOnClickListener(new View.OnClickListener() {
